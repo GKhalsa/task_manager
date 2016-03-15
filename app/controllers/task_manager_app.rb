eee5bc262b2e7393440f37d8dd700f3879e28c41
Gurusundesh Khalsa <gurusundesh@khalsa.com>
2016-03-15 12:39:46 -0600

    starting on saving tasks

--- app/controllers/task_manager_app.rb
+++ app/controllers/task_manager_app.rb
@@ -4,4 +4,24 @@ class TaskManagerApp < Sinatra::Base
   get '/' do
     erb :dashboard
   end
+
+  get '/tasks' do
+    @tasks = ["task1","task2","task3"]
+    erb :index
+  end
+
+  get '/tasks/new' do
+    erb :new
+  end
+
+  post '/tasks' do
+    task_manager.create(params[:task])
+    redirect '/tasks'
+    # "<p>Params: #{params}</p> <p>Task params: #{params[:task]}</p>"
+  end
+
+  def task_manager
+    database = YAML::Store.new('db/task_manager')
+    @task_manager ||= TaskManager.new(database)
+  end
 end
